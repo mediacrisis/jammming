d87b2cbda8e2fee92963891cbeb31999115db1f5
mediacrisis <mediacrisis@gmail.com>
2024-02-18 12:43:47 -0500

    Add cover images

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -5,10 +5,13 @@ import TrackButton from '../TrackButton/TrackButton';
 const Track = (props) => {
 	return (
 		<div className={styles.track}>
-			<div>
-				<p className={styles.title}>{props.track.name}</p>
-				<p className={styles.artist}>{props.track.artist}</p>
-				<p className={styles.album}>{props.track.album}</p>
+			<div className={styles.trackInner}>
+				<div><img src={props.track.cover_img[2].url} alt={props.track.album} /></div>
+				<div>
+					<p className={styles.title}>{props.track.name}</p>
+					<p className={styles.artist}>{props.track.artist}</p>
+					<p className={styles.album}>{props.track.album}</p>
+				</div>
 			</div>
 			<TrackButton btnType={props.btnType} handleClick={props.btnClick} track={props.track} />
 		</div>
